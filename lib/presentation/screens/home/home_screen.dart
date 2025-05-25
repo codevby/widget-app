73911ff473bfb2184d3e7563668dd57ba1691c40
Leonardo <leonardogarcarr23@gmail.com>
2025-05-25 16:52:21 -0400

    home screen implemented

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,10 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text('Home Screen')));
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Screen')),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final menuItems = appMenuItems;
+    return ListView.builder(
+      itemCount: menuItems.length,
+      itemBuilder: (context, index) {
+        final menuItem = menuItems[index];
+        return _CustomListTile(menuItem: menuItem);
+      },
+    );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile({super.key, required this.menuItem});
+
+  final MenuItems menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ListTile(
+      leading: Icon(menuItem.icon, color: colors.primary),
+      trailing: Icon(Icons.arrow_forward_ios, color: colors.primary),
+      title: Text(menuItem.title),
+      subtitle: Text(menuItem.subtitle),
+      onTap: () {
+        // Navigator.pushNamed(context, menuItem.link);
+      },
+    );
   }
 }
